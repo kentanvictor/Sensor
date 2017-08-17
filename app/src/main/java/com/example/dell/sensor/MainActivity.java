e1a44e85f1eef43cfa1385c5828ae2ed1810296c
kentanvictor <2030457926@qq.com>
2017-08-17 12:09:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/sensor/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.dell.sensor;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView sensor;
+    private SensorManager manager;
+    private Sensor magneticSensor, accelerometerSensor;
+    private SensorEventListener listener = new SensorListener();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensor = (ImageView) this.findViewById(R.id.img);
+        sensor.setKeepScreenOn(true);
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        magneticSensor =  manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        accelerometerSensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        manager.registerListener(listener,magneticSensor,SensorManager.SENSOR_DELAY_GAME);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        manager.unregisterListener(listener);
+        super.onPause();
+    }
+
+    private final class SensorListener implements SensorEventListener{
+        private float predegree = 0;
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float degree =  event.values[0];//存放了方向值
+            RotateAnimation animation = new RotateAnimation(predegree,-degree,
+                    Animation.RELATIVE_TO_SELF, 0.5f,Animation.RELATIVE_TO_SELF, 0.5f);
+            animation.setDuration(200);
+            sensor.startAnimation(animation);
+            predegree = -degree;
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    }
+}
