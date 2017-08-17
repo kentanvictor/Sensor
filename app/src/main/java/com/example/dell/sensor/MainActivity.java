a1ec1a2f65e023e30ec37cd2abd0b81006d9bac3
kentanvictor <2030457926@qq.com>
2017-08-17 12:43:38 +0800

    third commit

--- app/src/main/java/com/example/dell/sensor/MainActivity.java
+++ app/src/main/java/com/example/dell/sensor/MainActivity.java
@@ -7,30 +7,42 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView sensor;
+    private TextView tv;
     private SensorManager manager;
     private Sensor magneticSensor, accelerometerSensor;
     private SensorEventListener listener = new SensorListener();
+    private float[] values, r, gravity, geomagnetic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sensor = (ImageView) this.findViewById(R.id.img);
+        tv = (TextView) this.findViewById(R.id.tv);
         sensor.setKeepScreenOn(true);
         manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        //获取SensorManager
+        manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        listener = new SensorListener();
+        //获取Sensor
+        magneticSensor = manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        accelerometerSensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //初始化数组
+        values = new float[3];//用来保存最终的结果
+        gravity = new float[3];//用来保存加速度传感器的值
+        r = new float[9];//
+        geomagnetic = new float[3];//用来保存地磁传感器的值
     }
 
     @Override
     protected void onResume() {
-        magneticSensor = manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        accelerometerSensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        manager.registerListener(listener, magneticSensor, SensorManager.SENSOR_DELAY_GAME);
+        manager.registerListener(listener, magneticSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        manager.registerListener(listener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
         super.onResume();
     }
 
@@ -42,20 +54,35 @@ public class MainActivity extends AppCompatActivity {
 
     private final class SensorListener implements SensorEventListener {
         private float predegree = 0;
-        private float gravity = 0;
         @Override
         public void onSensorChanged(SensorEvent event) {
-            float degree = event.values[0];//存放了方向值
-            RotateAnimation animation = new RotateAnimation(predegree, -degree,
-                    Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-            animation.setDuration(200);
-            sensor.startAnimation(animation);
-            predegree = -degree;
+            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                geomagnetic = event.values;
+            }
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                gravity = event.values;
+                getValue();
+            }
         }
 
         @Override
         public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
         }
+    }
+    public void getValue() {
+        // r从这里返回
+        SensorManager.getRotationMatrix(r, null, gravity, geomagnetic);
+        //values从这里返回
+        SensorManager.getOrientation(r, values);
+        //提取数据
+        double azimuth = Math.toDegrees(values[0]);
+        if (azimuth<0) {
+            azimuth=azimuth+360;
+        }
+        double pitch = Math.toDegrees(values[1]);
+        double roll = Math.toDegrees(values[2]);
+        tv.invalidate();
+        tv.setText("Azimuth：" + (int)azimuth + "\nPitch：" + (int)pitch + "\nRoll：" + (int)roll);
     }
 }
